372bd26082e3e39d0a67f2f4fee60b598136e34e
Pankaj Seervi A <pankajseervi2604@gmail.com>
2025-03-08 17:16:30 +0530

    worked on homepage finished json data accessing

--- lib/page_routes/home_route.dart
+++ lib/page_routes/home_route.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:barkly/page_routes/pet_purchase.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:shimmer/shimmer.dart';
 
 class HomeRoute extends StatefulWidget {
   const HomeRoute({super.key});
@@ -11,6 +16,26 @@ class HomeRoute extends StatefulWidget {
 
 class _HomeRouteState extends State<HomeRoute> {
   int currentIndex = 0;
+  List petData = [];
+  Set<int> likedPets = {};
+
+  // to get the data first
+  @override
+  void initState() {
+    super.initState();
+    jsonDataFetching();
+  }
+
+  Future jsonDataFetching() async {
+    final String jsonData = await rootBundle.loadString("assets/pet_data.json");
+    final Map<String, dynamic> jsonMap = jsonDecode(jsonData);
+
+    setState(() {
+      // converting map to list
+      petData = jsonMap["pets"];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final List categories = ["Filter", "Dogs", "Cats", "Rabbits"];
@@ -175,109 +200,141 @@ class _HomeRouteState extends State<HomeRoute> {
           Expanded(
             child: Padding(
               padding: EdgeInsets.only(left: 10.r, right: 10.r),
-              child: GridView.builder(
-                padding: EdgeInsets.all(8),
-                itemCount: 12,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 12,
-                  mainAxisSpacing: 12,
-                  mainAxisExtent: 245,
-                ),
-                itemBuilder: (context, index) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      color: Colors.purple[50],
-                      borderRadius: BorderRadius.circular(16.r),
-                    ),
-                    child: Column(
-                      children: [
-                        ClipRRect(
-                          borderRadius:
-                              BorderRadius.vertical(top: Radius.circular(12.r)),
-                          child: Image.asset(
-                            "assets/images/sys_img/demo dog.jpeg",
+              child: petData != null
+                  ? GridView.builder(
+                      padding: EdgeInsets.all(8),
+                      itemCount: petData.length,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 12,
+                        mainAxisSpacing: 12,
+                        mainAxisExtent: 255,
+                      ),
+                      itemBuilder: (context, index) {
+                        final pet = petData[index];
+                        final isLiked = likedPets.contains(index);
+                        return Container(
+                          decoration: BoxDecoration(
+                            color: Colors.purple[50],
+                            borderRadius: BorderRadius.circular(16.r),
                           ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 5.r, right: 5.r),
-                          child: SizedBox(
-                            width: double.infinity,
-                            height: 70.h,
-                            child: Column(
-                              children: [
-                                Row(
-                                  children: [
-                                    Expanded(
-                                      child: Row(
+                          child: Column(
+                            children: [
+                              ClipRRect(
+                                  borderRadius: BorderRadius.vertical(
+                                      top: Radius.circular(12.r)),
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                          builder: (context) => PetPurchase(),
+                                        ),
+                                      );
+                                    },
+                                    child: Image.network(
+                                      pet['pet_image'],
+                                      fit: BoxFit.cover,
+                                      height: 175,
+                                      width: double.infinity,
+                                    ),
+                                  )),
+                              Padding(
+                                padding: EdgeInsets.only(left: 5.r, right: 5.r),
+                                child: SizedBox(
+                                  width: double.infinity,
+                                  height: 70.h,
+                                  child: Column(
+                                    children: [
+                                      Row(
                                         children: [
-                                          SizedBox(
-                                            width: 5.w,
-                                          ),
-                                          Text(
-                                            "Coco",
-                                            style: TextStyle(
-                                              fontSize: 16.sp,
-                                              fontWeight: FontWeight.w700,
+                                          Expanded(
+                                            child: Row(
+                                              children: [
+                                                SizedBox(
+                                                  width: 5.w,
+                                                ),
+                                                Text(
+                                                  pet['pet_name'],
+                                                  style: TextStyle(
+                                                    fontSize: 16.sp,
+                                                    fontWeight: FontWeight.w700,
+                                                  ),
+                                                ),
+                                                SizedBox(
+                                                  width: 5.w,
+                                                ),
+                                                pet['pet_gender'] == "male"
+                                                    ? Icon(
+                                                        Icons.male,
+                                                        color: Colors.green,
+                                                      )
+                                                    : Icon(
+                                                        Icons.female,
+                                                        color: Colors.pink,
+                                                      ),
+                                              ],
                                             ),
                                           ),
-                                          SizedBox(
-                                            width: 5.w,
-                                          ),
-                                          Icon(
-                                            Icons.male,
-                                            color: Colors.green,
+                                          IconButton(
+                                            onPressed: () {
+                                              setState(() {
+                                                if (isLiked) {
+                                                  likedPets.remove(index);
+                                                } else {
+                                                  likedPets.add(index);
+                                                }
+                                              });
+                                            },
+                                            icon: Icon(
+                                              isLiked
+                                                  ? Icons.favorite
+                                                  : Icons.favorite_border,
+                                              color: isLiked
+                                                  ? Colors.purple
+                                                  : Colors.purple,
+                                            ),
                                           ),
                                         ],
                                       ),
-                                    ),
-                                    IconButton(
-                                      onPressed: () {},
-                                      icon: Icon(
-                                        Iconsax.heart,
-                                        color: Colors.purple,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                SizedBox(
-                                  height: 20,
-                                  child: Row(
-                                    children: [
                                       SizedBox(
-                                        width: 5.w,
-                                      ),
-                                      Text(
-                                        "Adult",
-                                        style: TextStyle(
-                                          fontSize: 12.sp,
-                                        ),
-                                      ),
-                                      VerticalDivider(
-                                        thickness: 1,
-                                        color: Colors.grey,
-                                        endIndent: 5,
-                                        indent: 5,
-                                        width: 15,
-                                      ),
-                                      Text(
-                                        "demobread",
-                                        style: TextStyle(
-                                          fontSize: 12.sp,
+                                        height: 20,
+                                        child: Row(
+                                          children: [
+                                            SizedBox(
+                                              width: 5.w,
+                                            ),
+                                            Text(
+                                              pet["pet_age"],
+                                              style: TextStyle(
+                                                fontSize: 11.sp,
+                                              ),
+                                            ),
+                                            VerticalDivider(
+                                              thickness: 1,
+                                              color: Colors.grey,
+                                              endIndent: 5,
+                                              indent: 5,
+                                              width: 15,
+                                            ),
+                                            Text(
+                                              pet['pet_breed'],
+                                              style: TextStyle(
+                                                fontSize: 11.sp,
+                                              ),
+                                            ),
+                                          ],
                                         ),
                                       ),
                                     ],
                                   ),
                                 ),
-                              ],
-                            ),
+                              ),
+                            ],
                           ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
+                        );
+                      },
+                    )
+                  : CircularProgressIndicator(),
             ),
           ),
         ],
---
+++ lib/page_routes/pet_purchase.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PetPurchase extends StatelessWidget {
+  const PetPurchase({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
